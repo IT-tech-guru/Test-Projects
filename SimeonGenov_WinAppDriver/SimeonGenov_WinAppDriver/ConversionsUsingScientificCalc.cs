aad6cba7edd045a8532a3b42286bea398da87c47
IT-tech-guru <59871835+IT-tech-guru@users.noreply.github.com>
2020-12-23 03:54:51 +0200

    WinApp tests - introducing Page Objects

---
+++ SimeonGenov_WinAppDriver/SimeonGenov_WinAppDriver/ConversionsUsingScientificCalc.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using SimeonGenov_WinAppDriver.Views;
+
+namespace SimeonGenov_WinAppDriver
+{
+    class ConversionsUsingScientificCalc
+    {
+        private WindowsDriver<WindowsElement> _driver;
+        private CalculatorStandardView _calcStandardView;
+
+        [SetUp]
+        public void TestInit()
+        {
+            var options = new AppiumOptions();
+
+            options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            _calcStandardView = new CalculatorStandardView(_driver);
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            _driver?.Quit();
+        }
+
+        [Test]
+        [TestCase(2, "Converts into 0.000071 Cubic feet")]
+        [TestCase(3, "Converts into 0.000106 Cubic feet")]
+        public void TestConvertCubicCentimetersToCubicFeetOneDigit(int num1, string result)
+        {
+            _calcStandardView.ConvertCubicCentimetersToCubicFeetOneDigit(num1);
+            _calcStandardView.AssertResult(result);
+
+        }
+
+        [Test]
+        [TestCase(2, 5, "Converts into 0.000883 Cubic feet")]
+        [TestCase(3, 6, "Converts into 0.001271 Cubic feet")]
+        public void TestConvertCubicCentimetersToCubicFeetTwoDigit(int num1, int num2, string result)
+        {
+            _calcStandardView.ConvertCubicCentimetersToCubicFeetTwoDigit(num1, num2);
+            _calcStandardView.AssertResult(result);
+
+        }
+
+        [Test]
+        [TestCase(2, 5, 8, "Converts into 0.009111 Cubic feet")]
+        [TestCase(3, 6, 9, "Converts into 0.013031 Cubic feet")]
+        public void TestConvertCubicCentimetersToCubicFeetThreeDigit(int num1, int num2, int num3, string result)
+        {
+            _calcStandardView.ConvertCubicCentimetersToCubicFeetThreeDigit(num1, num2, num3);
+            _calcStandardView.AssertResult(result);
+
+        }
+
+
+        [Test]
+        [TestCase(2, "Converts into 35.6 Fahrenheit")]
+        [TestCase(3, "Converts into 37.4 Fahrenheit")]
+        public void TestConvertCelsiusToFahrenHeitOneDigit(int num1, string result)
+        {
+            _calcStandardView.ConvertCelsiusToFahrenHeitOneDigit(num1);
+            _calcStandardView.AssertResult(result);
+
+        }
+
+        [Test]
+        [TestCase(2, 5, "Converts into 77 Fahrenheit")]
+        [TestCase(3, 6, "Converts into 96.8 Fahrenheit")]
+        public void TestConvertCelsiusToFahrenHeitTwoDigit(int num1, int num2, string result)
+        {
+            _calcStandardView.ConvertCelsiusToFahrenHeitTwoDigit(num1, num2);
+            _calcStandardView.AssertResult(result);
+
+        }
+
+        [Test]
+        [TestCase(2, 5, 8, "Converts into 496.4 Fahrenheit")]
+        [TestCase(3, 6, 9, "Converts into 696.2 Fahrenheit")]
+        public void TestConvertCelsiusToFahrenHeitThreeDigit(int num1, int num2, int num3, string result)
+        {
+            _calcStandardView.ConvertCelsiusToFahrenHeitThreeDigit(num1, num2, num3);
+            _calcStandardView.AssertResult(result);
+
+        }
+    }
+}
---
+++ SimeonGenov_WinAppDriver/SimeonGenov_WinAppDriver/Views/StandardView.Actions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace SimeonGenov_WinAppDriver.Views
+{
+    public partial class CalculatorStandardView
+    {
+        private readonly WindowsDriver<WindowsElement> _driver;
+
+        public CalculatorStandardView(WindowsDriver<WindowsElement> driver) => _driver = driver;
+
+        public void PerformCalculation(int num1, char operation, int num2)
+        {
+            ClickByDigit(num1);
+            PerformOperations(operation);
+            ClickByDigit(num2);
+            EqualsButton.Click();
+        }
+
+        public void ConvertCelsiusToFahrenHeitOneDigit(int num1)
+        {
+            PrepareConversionCelsiusToFahrenHeit();
+            ClickByDigit(num1);
+        }
+
+        public void ConvertCelsiusToFahrenHeitTwoDigit(int num1, int num2)
+        {
+            PrepareConversionCelsiusToFahrenHeit();
+            ClickByDigit(num1);
+            ClickByDigit(num2);
+        }
+        public void ConvertCelsiusToFahrenHeitThreeDigit(int num1, int num2, int num3)
+        {
+            PrepareConversionCelsiusToFahrenHeit();
+            ClickByDigit(num1);
+            ClickByDigit(num2);
+            ClickByDigit(num3);
+        }
+
+        public void PrepareConversionCelsiusToFahrenHeit()
+        {
+            Navigation.Click();
+            TemperatureConverter.Click();
+            InputUnit.Click();
+            Celsius.Click();
+            OutputUnit.Click();
+            Fahrenheit.Click();
+        }
+
+        public void ConvertCubicCentimetersToCubicFeetOneDigit(int num1)
+        {
+            PrepareConversionCubicCentimetersToCubicFeet();
+            ClickByDigit(num1);
+        }
+
+        public void ConvertCubicCentimetersToCubicFeetTwoDigit(int num1, int num2)
+        {
+            PrepareConversionCubicCentimetersToCubicFeet();
+            ClickByDigit(num1);
+            ClickByDigit(num2);
+        }
+
+        public void ConvertCubicCentimetersToCubicFeetThreeDigit(int num1, int num2, int num3)
+        {
+            PrepareConversionCubicCentimetersToCubicFeet();
+            ClickByDigit(num1);
+            ClickByDigit(num2);
+            ClickByDigit(num3);
+        }
+
+        public void PrepareConversionCubicCentimetersToCubicFeet()
+        {
+            Navigation.Click();
+            VolumeConverter.Click();
+            InputUnit.Click();
+            CubicCentimeters.Click();
+            OutputUnit.Click();
+            CubicFeet.Click();
+        }
+
+        private void ClickByDigit(int digit)
+        {
+            switch (digit)
+            {
+                case 1:
+                    OneButton.Click();
+                    break;
+                case 2:
+                    TwoButton.Click();
+                    break;
+                case 3:
+                    ThreeButton.Click();
+                    break;
+                case 4:
+                    FourButton.Click();
+                    break;
+                case 5:
+                    FiveButton.Click();
+                    break;
+                case 6:
+                    SixButton.Click();
+                    break;
+                case 7:
+                    SevenButton.Click();
+                    break;
+                case 8:
+                    EightButton.Click();
+                    break;
+                case 9:
+                    NineButton.Click();
+                    break;
+                default:
+                    throw new NotSupportedException($"Not Supported digit = {digit}");
+            }
+        }
+
+        private void PerformOperations(char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    PlusButton.Click();
+                    break;
+                case '-':
+                    MinusButton.Click();
+                    break;
+                case '=':
+                    EqualsButton.Click();
+                    break;
+                case '*':
+                    MultiplyByButton.Click();
+                    break;
+                case '/':
+                    DivideButton.Click();
+                    break;
+                default:
+                    throw new NotSupportedException($"Not Supported operation = {operation}");
+            }
+        }
+
+        //private string GetCalculatorResultText() => 
+        //    ResultsInput.Text.Replace("Display is", string.Empty).Trim();
+
+        private string GetCalculatorResultText()
+        {
+            return _driver.FindElementByAccessibilityId("Value2").Text.Replace("Display is", string.Empty).Trim();
+        }
+    }
+}
